58c921e73e82fbc6e7f70f9de9bc4416a12bb553
Ajindoraptor <144355051+Ajindoraptor@users.noreply.github.com>
2023-09-26 10:11:18 -0400

    Update Loops.py

--- Loops.py
+++ Loops.py
@@ -1,1 +1,18 @@
+import math
 
+numbers = [4, 9, 16, 25, 36]
+
+for num in numbers:
+    square_root = math.sqrt(num)
+    print(f"The square root of {num} is {square_root}")
+
+numbers = [4, 9, 16, 25, 36]
+
+index = 0
+
+while index < len(numbers):
+    cubed_value = numbers[index] ** 3
+
+    print(f"The cubed value of {numbers[index]} is {cubed_value}")
+    
+    index += 1
