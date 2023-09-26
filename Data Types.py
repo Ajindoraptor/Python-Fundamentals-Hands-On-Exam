e7db7976cba6a82fba43583b9a87aeaf13ecc720
Ajindoraptor <144355051+Ajindoraptor@users.noreply.github.com>
2023-09-26 09:50:43 -0400

    Update Data Types.py

--- Data Types.py
+++ Data Types.py
@@ -1,1 +1,35 @@
+intro = "Hello, my name is "
+first_name = "Alex"
 
+full_intro = intro + first_name
+print(full_intro)
+
+#intro = "Hello World"
+#print(intro)
+
+#first_name = input("What is your first name: ")
+#print("name:", first_name)
+
+age = 17  
+temperature = 59 
+too_hot = False  
+too_cold = True
+
+print("Age:", age)
+print("Temperature:", temperature)
+print("Too Hot:", too_hot)
+print("Too Cold:", too_cold)
+
+addition = age + temperature
+subtraction = age - temperature
+multiplication = age * temperature
+division = age / temperature
+square_age = age ** 2
+square_temperature = temperature ** 2
+
+print("Age + Temperature =", addition)
+print("Age - Temperature =", subtraction)
+print("Age * Temperature =", multiplication)
+print("Age / Temperature =", division)
+print("Age squared =", square_age)
+print("Temperature squared =", square_temperature)
