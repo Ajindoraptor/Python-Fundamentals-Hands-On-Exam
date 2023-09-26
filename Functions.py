24e3b9397fa1a6d24bbed66a83117324c061536f
Ajindoraptor <144355051+Ajindoraptor@users.noreply.github.com>
2023-09-26 10:02:40 -0400

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -1,1 +1,23 @@
+def calculate_area(length, width):
+    area = length * width
+    return area
 
+length = 5
+width = 10
+result = calculate_area(length, width)
+print("The area of the rectangle is:", result)
+
+length1 = 5
+width1 = 3
+area1 = calculate_area(length1, width1)
+print(f"The area of the rectangle with length {length1} and width {width1} is {area1}.")
+
+length2 = 7
+width2 = 4
+area2 = calculate_area(length2, width2)
+print(f"The area of the rectangle with length {length2} and width {width2} is {area2}.")
+
+length3 = 10
+width3 = 2.5
+area3 = calculate_area(length3, width3)
+print(f"The area of the rectangle with length {length3} and width {width3} is {area3}.")
